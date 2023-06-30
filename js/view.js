a2e006f4cfdb1751e1e3c84508a8ee781dc43e4e
H0p3er <130352504+H0p3er@users.noreply.github.com>
2023-06-30 09:33:05 +0700

    update view

--- js/view.js
+++ js/view.js
@@ -2,18 +2,21 @@ import { component } from "./component.js";
 const view = {};
 // Thay doi giao dien
 view.setScreen = (screenName) => {
+    
     switch (screenName){
         case 'homepageScreen':
-
             document.getElementById('app').innerHTML = component.navbar + component.header + component.homepage + component.footer;
+            document.getElementById('script').setAttribute('src','../js/login.js')
             break;
 
         case 'postScreen':
             document.getElementById('app').innerHTML =component.navbar + component.header + component.post + component.footer;
+            document.getElementById('script').setAttribute('src','../js/login.js')
             break;
         
         case 'registerScreen':
             document.getElementById('app').innerHTML = component.registerNavbar + component.register + component.footer;
+            document.getElementById('script').setAttribute('src','../js/register.js')
             break;
     }
 }
