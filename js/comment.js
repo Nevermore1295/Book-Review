f566869e7666129bc0ef7bdfcc4dc029e8c925b4
Nevermore1295 <104438178+Nevermore1295@users.noreply.github.com>
2026-07-18 15:30:42 +0700

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -5,12 +5,12 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-s
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAA--PpoUl0p1Pjp3vubEVej9axE9xsE-8",
-    authDomain: "book-review-ac765.firebaseapp.com",
-    projectId: "book-review-ac765",
-    storageBucket: "book-review-ac765.appspot.com",
-    messagingSenderId: "275710298289",
-    appId: "1:275710298289:web:b86e1cc2130d6ee8c17a90"
+  apiKey: "AIzaSyAA--PpoUl0p1Pjp3vubEVej9axE9xsE-8",
+  authDomain: "book-review-ac765.firebaseapp.com",
+  projectId: "book-review-ac765",
+  storageBucket: "book-review-ac765.appspot.com",
+  messagingSenderId: "275710298289",
+  appId: "1:275710298289:web:b86e1cc2130d6ee8c17a90"
 };
 
 // Initialize Firebase
